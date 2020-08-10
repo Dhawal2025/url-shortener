c54bccc2115eeee4aee08ff8e82f8a0e109822de
Dhawal Sharda <dsharda63@gmail.com>
2020-08-10 12:15:05 +0530

    backend connected to frontend and shortUrl schema designed

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./ models/shortUrl');
 
+const dbURI = 'mongodb+srv://dhawal:ZRg0BV1Ekj4Rynj7@nodetuts.oducf.mongodb.net/node-tuts?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI,{
+    useNewUrlParser:true, useUnifiedTopology:true
+})
 app.set('view engine','ejs');
-
-app.get('/',(req,res)=>{
-    res.render('index');
+app.use(express.urlencoded({ extended:false}));
+app.get('/', async (req,res)=>{
+    const shortUrls = await ShortUrl.find();
+    res.render('index',{shortUrls:shortUrls});
 });
 
-app.listen(process.env.PORT || 5000) ;
+app.post('/shortUrls', async (req,res)=>{
+    await ShortUrl.create({ full:req.body.fullUrl})
+    res.redirect('/');
+})
+
+app.get('/:shortUrl',async (req,res)=>{
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl});
+    if( shortUrl == null){
+        res.sendStatus(404)
+    }
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+})
+app.listen(process.env.PORT || 3000) ;
